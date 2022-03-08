4acdac56fc8419ee55ffcb3c2dba07a3b2432ef1
yxfeng-tw <yxfeng@thoughtworks.com>
2022-03-08 10:24:55 +0800

    test terraform apply with tfplan

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  subnet_id = "subnet-05c99b18033fa0f30"
+  subnet_id = "subnet-08019fbedb8865fd0"
 
   tags = {
     Name = var.instance_name
